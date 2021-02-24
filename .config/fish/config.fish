60f2d6be81193437357597a50f1ec9c20c03d4d1
Mora671 <gigomota671@gmail.com>
2021-02-23 19:04:09 -0600

    Apps config

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+neofetch
+set fish_greeting ""
+
+# Spacefish Confgs
+set SPACEFISH_PROMPT_ORDER user dir host git package node docker ruby golang php rust haskell julia aws conda pyenv kubecontext exec_time line_sep jobs exit_code char
+set SPACEFISH_RPROMPT_ORDER time
+
+set SPACEFISH_TIME_SHOW true 
+set SPACEFISH_USER_SHOW always
+set SPACEFISH_USER_COLOR green
+set SPACEFISH_USER_COLOR_ROOT red
+set SPACEFISH_CHAR_SYMBOL 
